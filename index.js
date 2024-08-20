f28161d4c4b88f5baf4a7410b0cf506c1e9e4624
Eman Asfaw <emanasfaw21@gmail.com>
2024-08-20 23:49:37 +0300

    first commit

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDb = require("./dbconfigs/dbs");
 const router = require("./routes/route");
-const cookieparser=require("cookie-parser")
-const {app,server}=require('./socket/index')
+const cookieparser = require("cookie-parser");
+const { app, server } = require('./socket/index');
+
 dotenv.config();
 
-// const app = express();
-
 app.use(express.json());
-app.use(cookieparser())
+app.use(cookieparser());
+
+// CORS Configuration
 app.use(cors({
   origin: process.env.FRONT_END_URL,
   credentials: true
@@ -20,6 +21,7 @@ const port = process.env.PORT || 8080;
 
 // API routing
 app.use('/api', router);
+
 connectDb().then(() => {
   server.listen(port, () => {
     console.log(`Server running on ${port}`);
@@ -27,6 +29,7 @@ connectDb().then(() => {
 }).catch((error) => {
   console.log("Database error", error);
 });
+
 
 
 
